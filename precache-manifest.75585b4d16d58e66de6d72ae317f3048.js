b270dbb3c70fd51edb709a70becdd487a7dffaab
Charlie Cooper <charliecooper98@gmail.com>
2019-08-10 13:06:45 +0100

    Updates

---
+++ precache-manifest.75585b4d16d58e66de6d72ae317f3048.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ddbe58be459930a9c2141ff283d489c5",
+    "url": "/cycling-workout/index.html"
+  },
+  {
+    "revision": "08f91b7ea0771edc1182",
+    "url": "/cycling-workout/static/js/2.a99b8d80.chunk.js"
+  },
+  {
+    "revision": "10456d802d3751a57d5c",
+    "url": "/cycling-workout/static/js/main.9c740ba6.chunk.js"
+  },
+  {
+    "revision": "15f6221056a722b14e86",
+    "url": "/cycling-workout/static/js/runtime~main.25421592.js"
+  }
+]);
